42733a5241578daec9b3e3cc01e5ea49d92d3e5b
Mahathi Amencherla <mahathiamencherla@mahathis-air.3520.dhcp.asu.edu>
2022-10-28 17:23:21 -0700

    final push

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ import os
 import ffmpeg
 import boto3
 import csv
+import shutil
 
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-event={'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-west-2', 'eventTime': '2022-10-28T08:11:20.197Z', 'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'AWS:AIDASJUR6HKG2QTMFIMDF'}, 'requestParameters': {'sourceIPAddress': '98.191.174.30'}, 'responseElements': {'x-amz-request-id': 'BG7F524BNPWVKJCN', 'x-amz-id-2': 'DpyCtmgSiOwCUSNZlmRS2gwvALbM/aZJ5BQBIGhq/KsL4c/kSavhmau7hv57xOxrYn2yGzetHVRc+8kX+Y4rYDcq61T+7UFL'}, 's3': {'s3SchemaVersion': '1.0', 'configurationId': '6055bd1e-bd36-481d-9c7d-a23875a1eb34', 'bucket': {'name': 'test-lambdatrig', 'ownerIdentity': {'principalId': 'A2O3XJ6IOHI26F'}, 'arn': 'arn:aws:s3:::test-lambdatrig'}, 'object': {'key': 'test_1.mp4', 'size': 3610877, 'eTag': 'd1a9323ab22f0b4ee4876652b00ab425', 'sequencer': '00635B8EA807521EE7'}}}]}
+# event={'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-west-2', 'eventTime': '2022-10-28T08:11:20.197Z', 'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'AWS:AIDASJUR6HKG2QTMFIMDF'}, 'requestParameters': {'sourceIPAddress': '98.191.174.30'}, 'responseElements': {'x-amz-request-id': 'BG7F524BNPWVKJCN', 'x-amz-id-2': 'DpyCtmgSiOwCUSNZlmRS2gwvALbM/aZJ5BQBIGhq/KsL4c/kSavhmau7hv57xOxrYn2yGzetHVRc+8kX+Y4rYDcq61T+7UFL'}, 's3': {'s3SchemaVersion': '1.0', 'configurationId': '6055bd1e-bd36-481d-9c7d-a23875a1eb34', 'bucket': {'name': 'test-lambdatrig', 'ownerIdentity': {'principalId': 'A2O3XJ6IOHI26F'}, 'arn': 'arn:aws:s3:::test-lambdatrig'}, 'object': {'key': 'test_1.mp4', 'size': 3610877, 'eTag': 'd1a9323ab22f0b4ee4876652b00ab425', 'sequencer': '00635B8EA807521EE7'}}}]}
 
 s3_client = boto3.client('s3',
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -21,7 +22,7 @@ dynamodb_client = boto3.client('dynamodb',
 input_bucket = 'proj2-input-bucket'
 output_bucket = 'proj2-output-bucket'
 dynamodb_table = 'Proj2-students'
-frames_path = 'frames/'
+frames_path = '/tmp/'
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
@@ -31,9 +32,9 @@ def open_encoding(filename):
 	return data
 
 def download_video_s3(video_name):
-	s3_client.download_file(Bucket=input_bucket, Key=video_name, Filename='download/'+video_name)
+	s3_client.download_file(Bucket=input_bucket, Key=video_name, Filename='/tmp/'+video_name)
 	
-	os.system("ffmpeg -i " + str('download/'+name[0]) + " -r 1 " + str(frames_path) + "image-%3d.jpeg")
+	os.system("ffmpeg -i " + str('/tmp/'+video_name) + " -r 1 " + str(frames_path) + "image-%3d.jpeg")
 
 def get_item(name,video_name):
 	# response = dynamodb_client.query(TableName=dynamodb_table, 
@@ -41,29 +42,41 @@ def get_item(name,video_name):
 	# print(response)
 	response = dynamodb_client.scan(TableName=dynamodb_table,
     IndexName='name-index')
-	result = response
+	# result = response
 	#print(response)
-	l=[]
+	value_list=[]
 	for item in response['Items']:
 		if item['name']['S']==name:
 			print(item)
 			for i in item.values():
 				print(i)
 				if 'S' in i:
-					l.append(i['S'])
-	print(l)
+					value_list.append(i['S'])
+	print("Value List:", value_list)
 	filename=video_name.split(".")[0]+".csv"
-	print(filename)
+	print("File name:", filename, video_name)
 	
 	with open(filename, 'w', newline='') as myfile:
 		wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-		wr.writerow(l)
+		wr.writerow(value_list)
 	
 	response=s3_client.upload_file(filename, output_bucket, filename)
-	print(response)
+	print("upload response: ", response)
+
+	# clear frames
+	for filename in os.listdir(frames_path):
+		file_path = os.path.join(frames_path, filename)
+		try:
+			if os.path.isfile(file_path) or os.path.islink(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print('Failed to delete %s. Reason: %s' % (file_path, e))
+
     
 
-def face_recognition_handler(event):	
+def face_recognition_handler(event, context):	
 
 	#encodes known faces and names
 	data = open_encoding('encoding')
@@ -90,8 +103,8 @@ def face_recognition_handler(event):
 				name = known_names[first_match_index]
 				break
 
-	print("Result:", name)
-	get_item(name,video_name[0])
+	print("Result of face recognition:", name)
+	get_item(name,video_name)
     
-face_recognition_handler(event)
+# face_recognition_handler(event)
 # download_video_s3()
